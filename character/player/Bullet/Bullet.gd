9863401964cb6ae97a16d2e1037ecd4c52c71c4e
djvilla <vdjvilla@outlook.com>
2020-03-20 08:57:35 -0500

    feat: added loot state and done event that ends the loot state

--- character/player/Bullet/Bullet.gd
+++ character/player/Bullet/Bullet.gd
@@ -1,6 +1,11 @@
 extends RigidBody2D
 
+#Determines how long a bullet travels before it is removed from the game
+export var bullet_life = 1.5 #Setting for 15 seconds
 
+func _process(delta):
+	yield(get_tree().create_timer(bullet_life), "timeout")
+	queue_free()
 
 func _on_Bullet_body_entered(body):
 	if !body.is_in_group("character"):
