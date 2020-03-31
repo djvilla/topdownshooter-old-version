5763db6bc71fa3ac729209a6aaff89f4ed45c674
djvilla <vdjvilla@outlook.com>
2020-03-30 19:48:23 -0500

    chore: new state label script file

---
+++ helpers/state-label-world.gd
@@ -0,0 +1,19 @@
+extends Label
+
+var Player = load("res://character/player/player.gd")
+
+var _state_text = {
+	Player.States.IDLE: "idle",
+	Player.States.WALK: "walk",
+	Player.States.RUN: "run",
+	Player.States.LOOT: "loot",
+	Player.States.SNEAK: "sneak",
+	Player.States.MELEE: "melee"
+}
+
+
+func setup(character):
+	character.connect("state_change", self, "_on_Character_state_changed")
+
+func _on_Player_state_change(state):
+	text = _state_text[state]
