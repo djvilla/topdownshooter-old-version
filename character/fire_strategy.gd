94439b05355e3c53d93630bf0ceeeecb6752551c
djvilla <vdjvilla@outlook.com>
2020-04-06 23:24:31 -0500

    feat: added gun parent

--- character/fire_strategy.gd
+++
@@ -1,6 +0,0 @@
-static func create_bullet_instance(bullet, bullet_spawn, bullet_speed, rotation, rotation_degrees):
-	var bullet_instance =bullet.instance()
-	bullet_instance.position = bullet_spawn.get_global_position()
-	bullet_instance.rotation_degrees = rotation_degrees
-	bullet_instance.apply_impulse(Vector2.ZERO, Vector2(bullet_speed, 0).rotated(rotation))
-	return bullet_instance
