23e9e1d55bd3553e25f5570d12c6215928f6c783
djvilla <vdjvilla@outlook.com>
2020-04-07 22:57:18 -0500

    refactor: did a workaround to change to a preferred sound within the gun object

--- character/weapon/gun/gun.gd
+++ character/weapon/gun/gun.gd
@@ -1,23 +1,30 @@
 extends Node2D
 
+var weapon_name = ""
+
 export var bullet_speed = 1000
 export var fire_rate = 0.2 # In seconds
+var noise_path = "res://sound/gun_revolver_pistol_shot_01.wav"
 var bullet = preload("res://character/bullet/Bullet.tscn")
-var can_fire = true #Helps to keep shots constant and not all at once
+var can_fire = true #Helps to keep shots constant and not shooting all at once
 
-
-func use_weapon(character, can_melee, bullet_spawn, bullet_noise):
-	if can_fire && can_melee:
-		var bullet_instance = _create_bullet_instance(bullet, bullet_spawn, bullet_speed, character.rotation, character.rotation_degrees)
+# Allows the character to fire the weapon when they can fire and when they are not meleeing
+func use_weapon(character, is_not_using_melee, bullet_spawn, bullet_noise):
+	if can_fire && is_not_using_melee:
+		var bullet_instance = _create_bullet_instance(bullet_spawn, character.rotation, character.rotation_degrees)
 		# Using properties above, spawn bullet
 		character.get_tree().get_root().add_child(bullet_instance)
-		#Animates here for smoke or flash
-		bullet_noise.play(0.1) # 0.1 skips that click at the begining of the audio clip
+		# Animation here for smoke or flash
+		# 
+		bullet_noise.stream = load(noise_path)
+		bullet_noise.play(0.1) # 0.1 skips that click at the beginning of the audio clip
 		can_fire = false
 		yield(character.get_tree().create_timer(fire_rate), "timeout")
 		can_fire = true
 
-func _create_bullet_instance(bullet, bullet_spawn, bullet_speed, rotation, rotation_degrees):
+# This function creates an instance of a bullet on the given bullet spawn.
+# matches the characters direction
+func _create_bullet_instance(bullet_spawn, rotation, rotation_degrees):
 	var bullet_instance = bullet.instance()
 	bullet_instance.position = bullet_spawn.get_global_position()
 	bullet_instance.rotation_degrees = rotation_degrees
